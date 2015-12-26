77a2a9b5f3b3b806eec4c4faba0a58f9a12c3c01
Timothy Wood <mail@woodtimothy.com>
2015-12-26 11:14:30 -0700

    Minor cleanup, added lookup rows and lookup cols lists (Lookup rows and lookup cols allows the program to quickly retrieve row and column indices corresponding to the key table without actually searching for the letter in the key table)

--- pyFair.py
+++ pyFair.py
@@ -25,9 +25,12 @@ DEBUGGING = True
 TEST_KEY = "DIFFERENT"
 
 LETTER_A = ord('A')
+NOT_IN_TABLE = 200 # LOOKUP_ROWS/COLS entry for letter not in key table
 HEIGHT = 5
 WIDTH = 5 # Table is a 5x5 grid
-TABLE = []
+TABLE = [] # key table (look up a letter given a row and col)
+LOOKUP_ROWS = [] # Reverse: look up row or column given a letter
+LOOKUP_COLS = []
 SKIP_LETTER = 'Q' # Default: table created skipping Q (Q's converted to X's)
                   # Alternative: skip J (J's converted to I's)
 
@@ -48,7 +51,7 @@ SKIP_LETTER = 'Q' # Default: table created skipping Q (Q's converted to X's)
 ###############################################################################
 
 def createTable(keyIn):
-    # Add letters of key to list of letters for key table
+    # Add letters of key to list of letters used to fill key table
     tableLetters = ""
     for char in keyIn:
         if char.isalpha():
@@ -62,13 +65,27 @@ def createTable(keyIn):
             # or if letter is (Q or J depending on option)
             tableLetters = tableLetters + letter
 
-    # Create a two-dimensional list - five rows
+    # Initialize LOOKUP_ROWS and LOOKUP_COLS
+    for i in range(0, 26):
+        LOOKUP_ROWS.append(NOT_IN_TABLE) # Entries for all but skipped letter
+        LOOKUP_COLS.append(NOT_IN_TABLE) #     will be replaced below
+
+    # Create a two-dimensional list - five rows of five, and fill with letters
     letterIndex = 0 #iterate over tableLetters to fill in key table
     for i in range(0, HEIGHT): # create five rows in table
         newRow = [] 
         for j in range(0, WIDTH):
-            newRow.append(tableLetters[letterIndex])
+            char = tableLetters[letterIndex]
+            newRow.append(char)
+
+            # Add entries to LOOKUP_ROWS and LOOKUP_COLS
+            lookupIndex = ord(char) - LETTER_A
+            print("CREATE TABLE lookupIndex:", lookupIndex)
+            LOOKUP_ROWS[lookupIndex] = i
+            LOOKUP_COLS[lookupIndex] = j
+
             letterIndex += 1
+        # end for j in range
         TABLE.append(newRow)
     # end for i in range
 
@@ -76,15 +93,25 @@ def printTable():
     for row in TABLE:
         print(row)
 
+def printLookupRows():
+    for i in range(0, len(LOOKUP_ROWS) ):
+        print(i, LOOKUP_ROWS[i])
+
+def printLookupCols():
+    for i in range(0, len(LOOKUP_COLS) ):
+        print(i, LOOKUP_COLS[i])
+
 ###############################################################################
 # Testing
 ###############################################################################
 
-print("LETTER_A is ", LETTER_A)
+print("Creating key table using key")
 createTable(TEST_KEY)
-print("Printing Rows")
-for row in TABLE:
-    print(row)
-#printTable()
+print("Printing Table")
+printTable()
+print("Printing Lookup Rows")
+printLookupRows()
+print("Printing Lookup Cols")
+printLookupCols()
 
 
